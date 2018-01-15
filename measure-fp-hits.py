1cd6298cd4c53cf4f9140ae1a3e6d87fa1452180
voddan <dgvodopyan@gmail.com>
2018-01-15 11:15:06 +0300

    Adapted molecule input system

--- measure-fp-hits.py
+++ measure-fp-hits.py
@@ -16,7 +16,6 @@ indigo = Indigo()
 renderer = IndigoRenderer(indigo)
 bingo = Bingo.createDatabaseFile(indigo, os.path.join('tempdb'), 'molecule', '')
 
-USE_RDKIT = False
 FP_SIZE_BYTES = 64
 THRESHOLD = 0.05
 MORGAN_RADIUS = 3
@@ -29,7 +28,7 @@ indigo.setOption("ignore-bad-valence", "true")
 indigo.setOption("render-output-format", "svg")
 indigo.setOption("similarity-type", SIMILARITY_TYPE)
 indigo.setOption("fp-sim-qwords", FP_SIZE_BYTES / 8)
-indigo.setOption("fp-ext-enabled", USE_RDKIT)
+# indigo.setOption("fp-ext-enabled", True)
 indigo.setOption("fp-ord-qwords", 0)  # optional
 indigo.setOption("fp-tau-qwords", 0)  # optional
 indigo.setOption("fp-any-qwords", 0)  # optional
@@ -56,63 +55,60 @@ def write_a_match(id: str, smiles: str, similarity: float, molecule, dir_path) -
         """ % (name, similarity, smiles)
 
 
-# RdKit FP
-if __name__ == '__main__' and USE_RDKIT:
-    name, smiles = root_mol_smiles[0]
-    molecule = indigo.loadMolecule(smiles)
-    fingerprint_rdkit = AllChem.GetMorganFingerprintAsBitVect(Chem.MolFromSmiles(smiles), MORGAN_RADIUS, FP_SIZE_BYTES * 8)
-    fingerprint_str = str(base64.b16encode(base64.b64decode(fingerprint_rdkit.ToBase64())))[2:-1].zfill(FP_SIZE_BYTES * 2)
-    fingerprint = molecule.fingerprintExt(fingerprint_str, FP_SIZE_BYTES)
-    rendering = str(renderer.renderToBuffer(molecule))
-
-    print(fingerprint_str)
-
-    path = os.path.join("..", "DATA", "pubchem_slice_100k.smiles")
-    file = open(path)
-    smiles_list = [l.rstrip() for l in file][:10000]
-    molecule_list = [Chem.MolFromSmiles(l) for l in smiles_list]
-
-    for sm in smiles_list:
-        m = indigo.loadMolecule(sm)
-        m_rdkit = Chem.MolFromSmiles(sm)
-        fp_rdkit = AllChem.GetMorganFingerprintAsBitVect(m_rdkit, MORGAN_RADIUS, FP_SIZE_BYTES * 8)
-        fp_str = str(base64.b16encode(base64.b64decode(fp_rdkit.ToBase64())))[2:-1].zfill(FP_SIZE_BYTES * 2)
-        fp_ext = m.fingerprintExt(fp_str, FP_SIZE_BYTES)
-        bingo.insertWithExtFP(m, fp_ext)
-
-    iterator = bingo.searchSimWithExtFP(molecule, THRESHOLD, 1.0, fingerprint, metric='tanimoto')
-    results = []
-
-    cur_mol = iterator.getIndigoObject()
-    while iterator.next():
-        sm = cur_mol.smiles()
-        mol = Chem.MolFromSmiles(sm)
-        r = str(renderer.renderToBuffer(cur_mol))
-        id = iterator.getCurrentId()
-        sim = iterator.getCurrentSimilarityValue()
-
-        results += [(id, sm, sim)]
-    iterator.close()
-
-    sorted_results = sorted(results, key=lambda result: result[2], reverse=True)
-
-    for res in sorted_results:
-        print("%.3f" % res[2])
-        print(res[1])
-
+# # RdKit FP
+# if __name__ == '__main__' and USE_RDKIT:
+#     name, smiles = root_mol_smiles[0]
+#     molecule = indigo.loadMolecule(smiles)
+#     fingerprint_rdkit = AllChem.GetMorganFingerprintAsBitVect(Chem.MolFromSmiles(smiles), MORGAN_RADIUS, FP_SIZE_BYTES * 8)
+#     fingerprint_str = str(base64.b16encode(base64.b64decode(fingerprint_rdkit.ToBase64())))[2:-1].zfill(FP_SIZE_BYTES * 2)
+#     fingerprint = molecule.fingerprintExt(fingerprint_str, FP_SIZE_BYTES)
+#     rendering = str(renderer.renderToBuffer(molecule))
+#
+#     print(fingerprint_str)
+#
+#     path = os.path.join("..", "DATA", "pubchem_slice_100k.smiles")
+#     file = open(path)
+#     smiles_list = [l.rstrip() for l in file][:10000]
+#     molecule_list = [Chem.MolFromSmiles(l) for l in smiles_list]
+#
+#     for sm in smiles_list:
+#         m = indigo.loadMolecule(sm)
+#         m_rdkit = Chem.MolFromSmiles(sm)
+#         fp_rdkit = AllChem.GetMorganFingerprintAsBitVect(m_rdkit, MORGAN_RADIUS, FP_SIZE_BYTES * 8)
+#         fp_str = str(base64.b16encode(base64.b64decode(fp_rdkit.ToBase64())))[2:-1].zfill(FP_SIZE_BYTES * 2)
+#         fp_ext = m.fingerprintExt(fp_str, FP_SIZE_BYTES)
+#         bingo.insertWithExtFP(m, fp_ext)
+#
+#     iterator = bingo.searchSimWithExtFP(molecule, THRESHOLD, 1.0, fingerprint, metric='tanimoto')
+#     results = []
+#
+#     cur_mol = iterator.getIndigoObject()
+#     while iterator.next():
+#         sm = cur_mol.smiles()
+#         mol = Chem.MolFromSmiles(sm)
+#         r = str(renderer.renderToBuffer(cur_mol))
+#         id = iterator.getCurrentId()
+#         sim = iterator.getCurrentSimilarityValue()
+#
+#         results += [(id, sm, sim)]
+#     iterator.close()
+#
+#     sorted_results = sorted(results, key=lambda result: result[2], reverse=True)
+#
+#     for res in sorted_results:
+#         print("%.3f" % res[2])
+#         print(res[1])
+#
 
 # Indigo FP
-if __name__ == '__main__' and not USE_RDKIT:
+if __name__ == '__main__':
     name, smiles = root_mol_smiles[0]
     molecule = indigo.loadMolecule(smiles)
     fingerprint = molecule.fingerprint("sim")
 
     path = os.path.join("..", "DATA", "pubchem_slice_100k.smiles")
-    file = open(path)
-    smiles_list = [l.rstrip() for l in file][:100]
-    molecule_list = [indigo.loadMolecule(l) for l in smiles_list]
 
-    for m in molecule_list:
+    for m in indigo.iterateSmilesFile(path):
         bingo.insert(m)
 
     iterator = bingo.searchSimWithExtFP(molecule, THRESHOLD, 1.0, fingerprint, metric='tanimoto')
